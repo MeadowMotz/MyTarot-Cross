442b3ce2c0c29a05de94a0549ba5161db0d1a7a6
Benjamin Motz <benjamin.motz@sinclair.edu>
2025-03-20 15:36:46 -0400

    Comment & add camera functionality

--- my_tarot_cross/lib/main.dart
+++ my_tarot_cross/lib/main.dart
@@ -7,7 +7,7 @@ import 'package:http/http.dart' as http;
 import 'package:network_info_plus/network_info_plus.dart';
 import 'package:logging/logging.dart'; 
 import 'package:my_tarot_cross/EditorPage.dart';
-import 'package:path_provider/path_provider.dart';
+import 'package:image_picker/image_picker.dart';
 
 void main() {
   Logger.root.level = Level.ALL;  
@@ -42,7 +42,6 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   static final Logger _logger = Logger('MyHomePage'); 
-  PlatformFile? _image;
   String? _path;
   String? _rectifiedImageBase64;
   bool isLoading = false;
@@ -52,7 +51,8 @@ class _MyHomePageState extends State<MyHomePage> {
   String selectedDeck = '';
   TextEditingController newDeckController = TextEditingController();
   TextEditingController newCardController = TextEditingController();
-  String cardName = '';
+  String? cardName;
+  final ImagePicker _picker = ImagePicker();
 
   Future<String?> _getIpAddress() async {
     if (Platform.isAndroid || Platform.isIOS) {
@@ -68,7 +68,6 @@ class _MyHomePageState extends State<MyHomePage> {
     if (result != null) {
       if (result.files.isNotEmpty) {
         setState(() {
-          _image = result.files.first;
           _path = result.files.first.path;
         });
       }
@@ -78,8 +77,7 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
-  Future<List<List<double>>> _getEdges(PlatformFile image) async {
-    String path = image.path!;
+  Future<List<List<double>>> _getEdges(String path) async {
     final bytes = await File(path).readAsBytes();
     final base64Image = base64Encode(bytes);
 
@@ -117,18 +115,17 @@ class _MyHomePageState extends State<MyHomePage> {
   void _navigateToEditor(BuildContext context) async {
     dynamic edges = await Navigator.push(
       context,
-      MaterialPageRoute(builder: (context) => EditorPage(imagePath: _path!, imageEdges: _getEdges(_image!))),
+      MaterialPageRoute(builder: (context) => EditorPage(imagePath: _path!, imageEdges: _getEdges(_path!))),
     );
-    _sendImageToBackend(_image!, edges);
+    _sendImageToBackend(_path!, edges);
   }
 
-  Future<void> _sendImageToBackend(PlatformFile image, List<List<double>>? edges) async {
+  Future<void> _sendImageToBackend(String path, List<List<double>>? edges) async {
     _logger.info("Starting manipulation...");
     setState(() {
-      isLoading = true; // Start loading
+      isLoading = true;
     });
 
-    String path = image.path!;
     _logger.info("Using image: $path");
     final bytes = await File(path).readAsBytes();
     final base64Image = base64Encode(bytes);
@@ -145,7 +142,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
 
     final response = await http.post(
-      Uri.parse('http://$ipAddress:5000/process_image'), // Using dynamic IP address
+      Uri.parse('http://$ipAddress:5000/process_image'), 
       headers: {'Content-Type': 'application/json'},
       body: jsonEncode(requestBody),
     );
@@ -164,7 +161,7 @@ class _MyHomePageState extends State<MyHomePage> {
     }
 
     setState(() {
-      isLoading = false; // Stop loading
+      isLoading = false; 
     });
   }
 
@@ -181,7 +178,6 @@ class _MyHomePageState extends State<MyHomePage> {
       return buildLoadingIndicator();
     } else if (_rectifiedImageBase64 != null && _rectifiedImageBase64!.isNotEmpty) {
       
-
       return SizedBox(
         width: 300,
         height: 400,
@@ -191,14 +187,13 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       );
     } else {
-      // If no rectified image is available, show an alternative message.
       return const Text('No rectified image available.', textAlign: TextAlign.center,);
     }
   }
 
   Future<void> _saveImage(String deck, String card) async {
     try {
-      String imagePath = '../decks/${deck}/';
+      String imagePath = '../decks/$deck/';
 
       // Create decks and deck folder if doesnt exist
       Directory folderDir = Directory(imagePath);
@@ -207,7 +202,7 @@ class _MyHomePageState extends State<MyHomePage> {
         await folderDir.create(recursive: true);
       }
    
-      File destinationFile = File(imagePath = imagePath + '$card.jpg');
+      File destinationFile = File(imagePath = '$imagePath$card.jpg');
 
       if (_rectifiedImageBase64 != null) {
         Uint8List imageBytes = base64Decode(_rectifiedImageBase64!);
@@ -218,6 +213,45 @@ class _MyHomePageState extends State<MyHomePage> {
     } catch (e) {
       _logger.severe('Error saving image: $e');
     }
+  }
+
+  Future<void> _openCamera() async {
+    final XFile? image = await _picker.pickImage(source: ImageSource.camera);
+
+    if (image != null) {
+      print('Camera image selected: ${image.path}');
+      _path = image.path;
+    } else {
+      print('Camera image selection canceled');
+    }
+  }
+
+  void _showDropdownMenu(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext ctx) {
+        return Wrap(
+          children: [
+            if (!Platform.isMacOS && !Platform.isLinux && !Platform.isWindows) ListTile(
+              leading: Icon(Icons.camera_alt),
+              title: Text('Camera'),
+              onTap: () {
+                Navigator.pop(ctx); // Close menu
+                _openCamera(); 
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.image),
+              title: Text('Choose File'),
+              onTap: () {
+                Navigator.pop(ctx); // Close menu
+                _pickImage(); 
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
@@ -231,42 +265,49 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center, 
           crossAxisAlignment: CrossAxisAlignment.center, 
           children: [
+            // Original image
             SizedBox(
               width: 300, 
-              child: _image != null ? Image.file(File(_path!)) : const Text("No image picked.", textAlign: TextAlign.center,),
+              child: _path != null ? Image.file(File(_path!)) : const Text("No image picked.", textAlign: TextAlign.center,),
             ),
-            SizedBox(width:50),
+            const SizedBox(width:50),
             Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
+                // Camera or file picker
                 ElevatedButton(
-                  onPressed: _pickImage,
+                  onPressed: () => _showDropdownMenu(context),
                   child: const Text('Pick Image'),
                 ),
-                if (_image != null) 
+                const SizedBox(height: 10,),
+                if (_path != null) 
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center, 
                     children: [
+                      // Manual editor
                       ElevatedButton(
                         onPressed: () => _navigateToEditor(context),
                         child: const Text('Manual'),
                       ),
                       const SizedBox(width: 20), 
+                      // Autodetect
                       ElevatedButton(
-                        onPressed: () => _sendImageToBackend(_image!, null),
+                        onPressed: () => _sendImageToBackend(_path!, null),
                         child: const Text('Auto detection'),
                       ),
                     ],
                   ),
-                SizedBox(height: 5,),
+                const SizedBox(height: 5,),
+                // When rectified image is available...
                 if (_rectifiedImageBase64!=null)
                   Row(
                   children: [
+                    // Select deck
                     DropdownButton<String>(
-                      value: selectedDeck.isEmpty ? null : selectedDeck,  // Set null if empty
+                      value: selectedDeck.isEmpty ? null : selectedDeck, 
                       onChanged: (String? newValue) {
                         setState(() {
-                          selectedDeck = newValue ?? '';  // Safely update selectedDeck
+                          selectedDeck = newValue ?? '';  
                         });
                       },
                       items: dropdownOptions.map<DropdownMenuItem<String>>((String value) {
@@ -284,7 +325,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       child: Row(
                         children: [
                           Container(
-                            width: 150, // Adjust this width based on your layout
+                            width: 150, 
                             child: TextField(
                               controller: newDeckController,
                               decoration: const InputDecoration(
@@ -298,10 +339,10 @@ class _MyHomePageState extends State<MyHomePage> {
                               String newDeck = newDeckController.text.trim();
                               if (newDeck.isNotEmpty && !dropdownOptions.contains(newDeck)) {
                                 setState(() {
-                                  dropdownOptions.insert(dropdownOptions.length - 1, newDeck); // Add before "Add new deck"
-                                  selectedDeck = newDeck; // Set it as the selected deck
+                                  dropdownOptions.insert(dropdownOptions.length - 1, newDeck); 
+                                  selectedDeck = newDeck; 
                                 });
-                                newDeckController.clear(); // Clear the input field
+                                newDeckController.clear(); 
                               } else {
                                 ScaffoldMessenger.of(context).showSnackBar(
                                   const SnackBar(content: Text('Please enter a valid deck name')),
@@ -314,8 +355,10 @@ class _MyHomePageState extends State<MyHomePage> {
                       ),
                     ),
                     const SizedBox(height: 20),
+                    // When deck selected and rectified image available ...
                     if (selectedDeck!='Add new deck' && _rectifiedImageBase64!=null)
                       Row( children: [
+                        // Enter card name
                         Container(
                           width: 200, // Adjust this width based on your layout
                           child: TextField(
@@ -325,17 +368,19 @@ class _MyHomePageState extends State<MyHomePage> {
                             ),
                           ),
                       ),
+                      // Save rectified image
                       ElevatedButton(
                         onPressed: () {
                           cardName = newCardController.text.trim();
                           newCardController.clear();
-                          _saveImage(selectedDeck, cardName!=null ? cardName : 'Card');
+                          _saveImage(selectedDeck, cardName!=null ? cardName! : 'Card');
                         },
                         child: Text("Save")),
                       ],),
                     ],
                   ),
-            SizedBox(width:50),
+            const SizedBox(width:50),
+            // Show rectified image
             SizedBox(
               width: 300,
               child: _displayRectifiedImage(),
--- my_tarot_cross/macos/Flutter/GeneratedPluginRegistrant.swift
+++ my_tarot_cross/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,11 +6,13 @@ import FlutterMacOS
 import Foundation
 
 import file_picker
+import file_selector_macos
 import network_info_plus_macos
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   NetworkInfoPlusPlugin.register(with: registry.registrar(forPlugin: "NetworkInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
